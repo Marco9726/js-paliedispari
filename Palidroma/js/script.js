fc202bb9dd829cb516fa26d00364174c08b10a94
Marco 9726 <marcosarra9@gmail.com>
2022-12-05 17:18:45 +0100

    esercizio finito

--- Palidroma/js/script.js
+++ Palidroma/js/script.js
@@ -1,26 +1,27 @@
 //1 - Creare un prompt per chiedere la parola all'utente
-const userWord = prompt('Scrivere una parola');
-//2- Trasformare la parola inserita in un array
-let myWord = userWord.split("")
-// //3 - Inverto l'ordine delle lettere dell'array creato
-// myWord.reverse
-// console.log(myWord);
-
-function confronto(firstWord, secondWord)
- {
-    secondWord = secondWord.reverse
-    secondWord = secondWord.join
+const userWord = prompt('Inserire una parola');
+//2 - creo la funzione di confronto
+function confronto(word) {
+    //2.1 creo una variabile e 2.2 trasformo la stringa in array e 2.3 inverto la posizione delle lettere e 2.4 la ritrasformo in stringa 
+    let secondWord = word.split("").reverse().join('')
     console.log(secondWord);
-    if ( `${firstWord} === ${secondWord}`)
-    {
-        alert('si')
+    //3 confronto le due parole
+    //3.1 - se le parole corrispondo resituisco 'True'
+    if ( word === secondWord){
+        return true
     }
-
-    else 
-    {
-        alert('no')
+    //3.2 - se le parole non corrispondo restituisco 'False'
+    else {
+        return false
     }
-    
 }
-
-confronto(userWord, myWord)
+//4 - invoco la funzione
+confronto(userWord)
+//4.1 - se la funzione mi restituisce 'True', creo un alert 'La parola inserita è palindroma'
+if (confronto(userWord)){
+    alert('La parola inserita è palindroma')
+}
+//4.2 - se la funzione mi restituisce 'False', creo un alert 'La parola inserita nonè palindroma'
+else {
+    alert('la parola inserita non è palindroma')
+}
--- Palidroma/readMe.txt
+++ Palidroma/readMe.txt
@@ -1,4 +1,13 @@
 Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 //1 - Creare un prompt per chiedere la parola all'utente
-//2- Trasformare la parola inserita in un array
-//3 - Inverto l'ordine delle lettere dell'array creato
+//2 - creo la funzione di confronto
+    //2.1 - creo una nuova variabile con il contenuto della parola inserita
+    //2.2 - trasformo la stringa in tale variabile in array
+    //2.3 - inverto la posizione delle lettere dell'array
+    //2.4 - ritrasformo il contenuto della variabile in stringa
+//3 - confronto le due variabili
+    //3.1 - se le parole corrispondo resituisco 'True'
+    //3.2 - se le parole non corrispondo restituisco 'False'
+//4 - Invoco la funzione
+    //4.1 - se la funzione mi restituisce 'True', creo un alert 'La parola inserita è palindroma'
+    //4.2 - se la funzione mi restituisce 'False', creo un alert 'La parola inserita nonè palindroma'
