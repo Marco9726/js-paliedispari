5f5e2e0f3486f39597169b6ae52be2b8bc85793f
Marco 9726 <marcosarra9@gmail.com>
2022-12-05 17:53:27 +0100

    ciclo do while iniziale

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -1,7 +1,7 @@
 //1 - Chiedo all'utente di scegliere pari o dispari
 let userNumber = ""
 do {
-     userNumber = prompt('Scegli pari o dispari')
-
-} while( userNumber != 'Pari' && userNumber != 'Dispari')
+     userNumber = prompt('Scegli pari o dispari').toLocaleLowerCase()
+//1.1 - far ripetere il prompt in caso scelga un'opzione che non sia pari o dispari
+} while( userNumber != 'pari' && userNumber != 'dispari')
 
